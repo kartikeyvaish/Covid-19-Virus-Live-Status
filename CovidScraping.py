db79fa56fcc304b47e07a07459f0ef54bfdf8fd3
Kartikey Vaish <55032587+kartikeyvaish@users.noreply.github.com>
2020-04-08 00:28:53 +0545

    Update CovidScraping.py

--- CovidScraping.py
+++ CovidScraping.py
@@ -4,38 +4,26 @@ import os
 import time
 
 
-def Pause(n):
-    time.sleep(n)
-
-
 def CLR():
     os.system("CLS")
 
 
-def CoronaReport():
-    a = list()
+def CoronaReport(a):
     print("Search for A Country for CORONA Report - ", end="")
     n = input()
     CLR()
-    page = requests.get('https://www.worldometers.info/coronavirus/#countries')
-    soup = BeautifulSoup(page.content, 'html.parser')
-    for record in soup.findAll('tr'):
-        b = list()
-        for data in record.findAll('td'):
-            if data.text == '' or data.text == ' ':
-                b.append(0)
-            else:
-                b.append(data.text)
-        a.append(b)
+    # This is just for Looks...
+    # page = requests.get('https://www.worldometers.info/coronavirus/#countries')
+    # soup = BeautifulSoup(page.content, 'html.parser')
+    # for record in soup.findAll('tr'):
+    #     b = list()
+    #     for data in record.findAll('td'):
+    #         if data.text == '' or data.text == ' ':
+    #             b.append(0)
+    #         else:
+    #             b.append(data.text)
+    #     a.append(b)
 
-    # print("Loading Results", end="", flush=True)
-    # time.sleep(1)
-    # print(".", end="", flush=True)
-    # time.sleep(1)
-    # print(".", end="", flush=True)
-    # time.sleep(1)
-    # print(".", end="", flush=True)
-    # time.sleep(1)
     CLR()
 
     for i in range(1, len(a)):
@@ -50,10 +38,10 @@ def CoronaReport():
             break
 
 
-def Driver():
+def Driver(a):
     ch = '1'
     while ch == '1':
-        CoronaReport()
+        CoronaReport(a)
         print("\r")
         print("Press 1 For More Search")
         print("Press 0 to Exit")
@@ -62,4 +50,16 @@ def Driver():
         CLR()
 
 
-Driver()
+page = requests.get('https://www.worldometers.info/coronavirus/#countries')
+soup = BeautifulSoup(page.content, 'html.parser')
+a=list()
+for record in soup.findAll('tr'):
+    b = list()
+    for data in record.findAll('td'):
+        if data.text == '' or data.text == ' ':
+            b.append(0)
+        else:
+            b.append(data.text)
+    a.append(b)
+
+Driver(a)
